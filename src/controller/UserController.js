cc057d5edeb8addc070f035b777515fa39ebe0d4
Dip_San <khadkasandip489@gmail.com>
2025-07-01 11:27:41 +0545

    Remove unwanted src folder from repo

--- src/controller/UserController.js
+++
@@ -1,9 +0,0 @@
-import UserServices from "../services/UserServices.js"
-
-const createUser = async (req,res) =>{
-    const {name,password,phone, email,address} = req.body
-
-    const data = await UserServices.createUser(req.body)
-}
-
-export {createUser}  //named export import garda ni curly braces ma halnu parxaaa
